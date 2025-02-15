35ab1e45223cb4509945fdfda23251b69cb50598
Mihneas22 <mihnea.serban06@gmail.com>
2025-02-15 21:58:19 +0200

    Add project files.

---
+++ Domain/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+            
+        public string? username { get; set; }
+
+        public string? email { get; set; }
+
+        public string? password { get; set; }
+
+        public string? city { get; set; }
+
+        public string? country { get; set; }
+
+        public int? points { get; set; }
+
+        public List<string>? itemsBoughtHistory { get; set; }
+
+        public List<string>? shoppingCart { get; set; }
+
+        public DateTime? createdAccount { get; set; }
+    }
+}
